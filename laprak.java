c67dc4b0e283295c7ec6e512526487c88adca2ac
Dapa <haqsyahdaffa@gmail.com>
2025-09-23 11:50:47 +0700

    Update and rename laprak to laprak.java

---
+++ laprak.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class laprak {
+  public static void main(String[] args) {
+   int operatorawal,operatorakhir;
+    Scanner input = new Scanner(System.in);
+    System.out.print("Masukkan operator pertama: ");
+    operatorawal = input.nextIn();
+    Systme.out.print("Masukkkan operator kedua: ");
+    operatorakhir = input.nextInt():
+      int penambahan = 
+    
